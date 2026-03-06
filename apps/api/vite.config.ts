3dee74681ad7c938d298da7d605a7fca1e72b08e
Thomas Lipman <tlipman@proton.me>
2026-03-06 12:31:31 +0100

    ..

--- apps/api/vite.config.ts
+++ apps/api/vite.config.ts
@@ -3,7 +3,26 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "node20",
-    outDir: "dist/api",
+    outDir: "dist",
     minify: "oxc",
+    lib: {
+      entry: "src/dev.ts",
+      formats: ["es"],
+      fileName: "dev",
+    },
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+        "@hono/node-server",
+        "hono",
+        "@hono/zod-validator",
+        "@repo/contract",
+        "@repo/db",
+        "@vercel/blob",
+        "bcrypt",
+        "sharp",
+        "zod",
+      ],
+    },
   },
 });
